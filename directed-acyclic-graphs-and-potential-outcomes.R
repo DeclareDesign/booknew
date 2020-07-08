9ac2ce4a269a5a27e712b9c58c1bce28a4b2b6bf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-08 18:46:18 +0000

    Deploy DeclareDesign/book to github.com/DeclareDesign/book.git:gh-pages

---
+++ directed-acyclic-graphs-and-potential-outcomes.R
@@ -0,0 +1,62 @@
+# ---
+# Directed Acyclic Graphs and Potential Outcomes
+# --- 
+
+packages <- c("knitr", "tidyverse", "DeclareDesign", "DesignLibrary")
+lapply(packages, require, character.only = T)
+
+# load packages for this section here. note many (DD, tidyverse) are already available, see scripts/package-list.R
+library(dagitty)
+library(ggdag)
+library(ggraph)
+
+a_blue <- "#0072B2"
+a_gray <- "grey80"
+
+dag <-
+  dagify(Y ~ X + Z + U,
+         X ~ U,
+         latent = "U")
+
+gg_df <-
+  tidy_dagitty(dag,
+               layout = "manual",
+               x = c(1, 0, -1, 1),
+               y = c(1, 1, 0, 0))
+
+gg_df <-
+  gg_df %>%
+  mutate(
+    color = case_when(
+      name == "U" ~ a_gray,
+      name == "X" ~ a_blue,
+      name == "Y" ~ a_blue,
+      name == "Z" ~ a_blue
+    )
+  )
+
+
+g <- 
+ggplot(gg_df, aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_dag_node(aes(color = color)) +
+  scale_color_identity() +
+  geom_dag_text(color = "black", family = "Helvetica") +
+  geom_dag_edges() +
+  theme_dag()
+
+g
+
+diff_in_cates <- 0.5
+design <-
+  declare_population(N = 100,
+                     U = rnorm(N),
+                     X = rbinom(N, 1, prob = pnorm(0.5 * U + rnorm(N)))) +
+  declare_potential_outcomes(Y ~ 0.5 * X + 0.5 * Z + diff_in_cates * X * Z + 0.5 * U) +
+  declare_estimands(
+    ATE = mean(Y_Z_1 - Y_Z_0),
+    CATE_1 = mean(Y_Z_1[X == 1] - Y_Z_0[X == 1]),
+    CATE_0 = mean(Y_Z_1[X == 0] - Y_Z_0[X == 0]),
+    DiC = mean(Y_Z_1[X == 1] - Y_Z_0[X == 1]) - mean(Y_Z_1[X == 0] - Y_Z_0[X == 0])
+  )
+
+design %>% draw_estimands %>% kable
