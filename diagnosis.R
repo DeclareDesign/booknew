c329491adfff229b90a3e20eb1e3c0b33a845364
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-10 21:26:00 +0000

    Deploy DeclareDesign/book to github.com/DeclareDesign/book.git:gh-pages

--- diagnosis.R
+++ diagnosis.R
@@ -6,3 +6,5 @@ packages <- c("knitr", "tidyverse", "DeclareDesign", "DesignLibrary")
 lapply(packages, require, character.only = T)
 
 # load packages for this section here. note many (DD, tidyverse) are already available, see scripts/package-list.R
+
+knitr::include_app('https://ushintsho.shinyapps.io/diagnosis/', height = '400px')
