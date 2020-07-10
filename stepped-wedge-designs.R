c84cebf86654579a4c9bb9bba4d3fce34083291e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-10 00:52:27 +0000

    Deploy DeclareDesign/book to github.com/DeclareDesign/book.git:gh-pages

--- stepped-wedge-designs.R
+++ stepped-wedge-designs.R
@@ -5,19 +5,16 @@
 packages <- c("knitr", "tidyverse", "DeclareDesign", "DesignLibrary")
 lapply(packages, require, character.only = T)
 
-p_00 <- p_W1 <- p_W2 <- p_W3 <- 1/4
+p_00 <- p_W1 <- p_W2 <- p_W3 <- 2/8
 
-design <- 
-  declare_population(
-    t = add_level(N = 3, u_t = rnorm(N), trend = as.numeric(t), 
-                  p = c(p_W1, p_W1 + p_W2, p_W1 + p_W2 + p_W3)),
-    i = add_level(N = 8, u_i = rnorm(N), nest = FALSE),
-    obs = cross_levels(by = join(t, i), u_ti = rnorm(N))) + 
-  declare_potential_outcomes(
-    Y_Z_0 = u_i + u_t + u_ti,
-    Y_Z_1 = u_i + u_t + u_ti + trend) + 
-  declare_assignment(clusters = i, 
-                     conditions = 1:4, 
+design <- declare_population(
+  t = add_level(N = 3, trend = as.numeric(t), 
+                # u_t = rnorm(N), 
+                p = c(p_W1, p_W1 + p_W2, p_W1 + p_W2 + p_W3)),
+  i = add_level(N = 8, u_i = rnorm(N), nest = FALSE),
+  obs = cross_levels(by = join(t, i), u_it = rnorm(N))) + 
+  declare_potential_outcomes(Y_Z_0 = u_i + u_it, Y_Z_1 = u_i + u_it + trend) +
+  declare_assignment(clusters = i, conditions = 1:4, 
                      prob_each = c(p_W1, p_W2, p_W3, p_00),
                      assignment_variable = "wave") + 
   declare_step(Z = as.numeric(t >= wave), 
@@ -25,24 +22,102 @@ design <-
                handler = fabricate) + 
   declare_reveal(Y, Z) + 
   declare_estimand(ate = mean(Y_Z_1 - Y_Z_0)) + 
-  declare_estimator(Y ~ Z, model = lm_robust, label = "Unweighted SW") +
-  declare_estimator(Y ~ Z, model = lm_robust, label = "Weighted SW", weights = ip) 
+  declare_estimator(Y ~ Z, model = lm_robust, label = "1: Wave 2 only", subset = t == 2) +
+  declare_estimator(Y ~ Z, model = lm_robust, label = "2: Weighted, clustered SW", weights = ip, clusters = i) +
+  declare_estimator(Y ~ Z, model = lm_robust, label = "3: Unweighted, unclustered SW") 
 
-draw_data(design) %>% 
-  mutate(i = fct_reorder(i, wave), Assignment = ifelse(Z == 1, "Treatment", "Control")) %>% 
+dat <- draw_data(design)
+
+treatment_color <- "royalblue2"
+control_color <- "darkorange3"
+
+sw_plot <- dat %>% 
+  arrange(wave,i) %>% 
+  group_by(t) %>%
+  mutate(i = 1:n(), Assignment = ifelse(Z == 1, "Treatment", "Control")) %>% 
   ggplot(aes(x = t, y = i, fill = Assignment)) +
-  geom_tile(color = "white") + scale_fill_grey(start = .9,end = .5) +
-  geom_text(aes(label = round(ip,1))) +
+  geom_tile(color = "white") + 
+  scale_fill_manual(values = c(control_color, treatment_color)) +
+  # scale_fill_grey(start = .9,end = .5) +
+  geom_text(aes(label = round(ip,2))) +
+  dd_theme() +
+  theme(legend.position = "right")
+
+
+
+ate_by_wave_plot <- dat %>% 
+  group_by(t) %>%
+  summarize(Y_Z_1 = mean(Y_Z_1), Y_Z_0 = mean(Y_Z_0)) %>% 
+  mutate(t = as.numeric(t),
+    ate = Y_Z_1 - Y_Z_0, 
+         label_y_position = Y_Z_0 + ate/2, 
+         label_x_position = t + .15,
+         label = paste0("ATE at wave ",t,":\n",ate)) %>% 
+  ggplot(aes(x = t)) +
+  geom_segment(aes(x = t, xend = t, y = Y_Z_0, yend = Y_Z_1), linetype = "dashed", alpha = .5) +
+  geom_line(aes(y = Y_Z_1), color = treatment_color) +
+  geom_point(aes(y = Y_Z_1), color = treatment_color) +
+  geom_line(aes(y = Y_Z_0), color = control_color) +
+  geom_point(aes(y = Y_Z_0), color = control_color) +
+  geom_text(aes(x = label_x_position, y = label_y_position, label = label)) +
+  geom_text(aes(x = label_x_position[3], y = Y_Z_1[3]), label = "E[Y(Z = 1)]", color = treatment_color) +
+  geom_text(aes(x = label_x_position[3], y = Y_Z_0[3]), label = "E[Y(Z = 0)]", color = control_color) +
+  scale_y_continuous("Average potential outcomes") +
+  scale_x_continuous("Wave", breaks = 1:3) +
   dd_theme()
 
 
-
-
-
-reshape_diagnosis(diagnoses) %>% kable()
+grid.arrange(sw_plot, ate_by_wave_plot,nrow = 1)
 
 
 
 
 
-reshape_diagnosis(sw_comparisons) %>% kable()
+
+diag_tab <- reshape_diagnosis(diagnosis) 
+diag_tab %>% select(c("Estimator Label", "Bias","Power","Coverage","Mean Estimate","SD Estimate","Mean Se")) %>% kable()
+
+designs <- list(
+  a = redesign(design, p_00 = 3/8, p_W1 =  3/8, p_W2 = 1/8, p_W3 = 1/8),
+  b = redesign(design, p_00 = 3/8, p_W1 =  2/8, p_W2 = 2/8, p_W3 = 1/8),
+  c = redesign(design, p_00 = 3/8, p_W1 =  2/8, p_W2 = 1/8, p_W3 = 2/8),
+  d = redesign(design, p_00 = 3/8, p_W1 =  1/8, p_W2 = 3/8, p_W3 = 1/8),
+  e = redesign(design, p_00 = 3/8, p_W1 =  1/8, p_W2 = 2/8, p_W3 = 2/8),
+  f = redesign(design, p_00 = 3/8, p_W1 =  1/8, p_W2 = 1/8, p_W3 = 3/8)
+  )
+
+
+
+
+
+designs_data <- names(designs) %>% 
+  lapply(function(name) designs[[name]] %>% 
+           draw_data %>% 
+           mutate(design_id = name)) %>% 
+  do.call(what = rbind,args = .) %>% 
+  arrange(design_id,wave,i) %>% 
+  group_by(t,design_id) %>%
+  mutate(i = 1:n(), Assignment = ifelse(Z == 1, "Treatment", "Control")) 
+
+
+diags <- diagnoses %>% 
+  get_diagnosands() %>% 
+  filter(estimator_label == "2: Weighted, clustered SW") %>% 
+  mutate(
+    design_name = paste0("Design ", design_label, ": W1 = ",8*p_W1,
+                         "; W2 = ", 8*p_W2, 
+                         "; W3 = ", 8*p_W3,",\nPower = ",round(power,2)
+                         
+                         )
+  )
+
+designs_data <- left_join(designs_data, diags %>% select(design_name, design_label), by = c("design_id" = "design_label"))
+
+designs_data %>% 
+  ggplot(aes(x = t, y = i, fill = Assignment)) +
+  geom_tile(color = "white") + 
+  scale_fill_manual(values = c(control_color, treatment_color)) +
+  dd_theme() + facet_wrap(~ design_name, nrow = 1) + 
+  scale_y_continuous("",labels = NULL)
+
+
