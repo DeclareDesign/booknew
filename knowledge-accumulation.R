a7b19bd627d3220791d38bf212f999fb6b588db0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-14 17:55:16 +0000

    Deploy DeclareDesign/book to github.com/DeclareDesign/book.git:gh-pages

--- knowledge-accumulation.R
+++
@@ -1,8 +0,0 @@
-# ---
-# Knowledge accumulation
-# --- 
-
-packages <- c("knitr", "tidyverse", "DeclareDesign", "DesignLibrary")
-lapply(packages, require, character.only = T)
-
-# load packages for this section here. note many (DD, tidyverse) are already available, see scripts/package-list.R
