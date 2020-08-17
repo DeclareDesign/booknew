4389df34b9c94aed294081e63c4f5807a136bccd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-17 23:23:33 +0000

    Deploy DeclareDesign/book to github.com/DeclareDesign/book.git:gh-pages

--- defining-the-inquiry.R
+++ defining-the-inquiry.R
@@ -7,10 +7,3 @@ lapply(packages, require, character.only = TRUE)
 
 # load packages for this section here. note many (DD, tidyverse) are already available, see scripts/package-list.R
 library(dagitty)
-
-
-plot(dagitty::graphLayout(dagitty("dag{ 
-  X -> Y ;
-  X -> M ;
-  M -> Y
- }")))
