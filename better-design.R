f340eec4f193fa9945f0a92dc4d7f53876039b70
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-08 23:04:12 +0000

    Deploy DeclareDesign/book to github.com/DeclareDesign/book.git:gh-pages

--- better-design.R
+++ better-design.R
@@ -26,9 +26,11 @@ simple_design <-
   #   it is equal to the unobserved shock 'u'
   # Y_Z_1 is the treated potential outcome 
   #   it is equal to the control potential outcome plus a treatment effect of 0.25
+  # declare_potential_outcomes(
+  #   Y_Z_0 = u, 
+  #   Y_Z_1 = Y_Z_0 + 0.25) +
   declare_potential_outcomes(
-    Y_Z_0 = u, 
-    Y_Z_1 = Y_Z_0 + 0.25) +
+    Y ~ 0.25 * Z + u) +
   
   # I: inquiry
   
@@ -61,6 +63,43 @@ simple_design <-
   # calculate the difference-in-means of Y depending on Z 
   # we link this estimator to ATE because this is our estimate of our inquiry
   declare_estimator(Y ~ Z, clusters = villages, model = difference_in_means, estimand = "ATE")
+
+
+design <-
+  declare_population(N = 100,
+                     X = rbinom(N, 1, 0.3),
+                     U = rnorm(N)) +
+  declare_potential_outcomes(Y ~ Z + X + U) +
+  declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0)) +
+  declare_assignment(blocks = X, block_prob = c(0.1, 0.5)) +
+  declare_estimator(Y ~ Z, estimand = "ATE", label = "Naive DIM") +
+  declare_estimator(Y ~ Z,
+                    blocks = X,
+                    estimand = "ATE",
+                    label = "Blocked DIM")
+
+dag <- dagify(
+  Y ~ Z + X + U,
+  Z ~ X
+)
+
+nodes <-
+  tibble(
+    name = c("Y", "Z", "U", "X"),
+    label = c("Y", "Z", "U", "X"),
+    annotation = c(
+      "**Outcome**<br>",
+      "**Random assignment**<br>",
+      "**Unknown heterogeneity**",
+      "**villages**<br>Used for cluster assignment"),
+    x = c(5, 1, 5, 1),
+    y = c(2.5, 2.5, 4, 4), 
+    nudge_direction = c("S", "S", "N", "N"),
+    answer_strategy = "uncontrolled"
+  )
+ggdd_df <- make_dag_df(dag, nodes, design)
+
+base_dag_plot %+% ggdd_df
 
 # Select diagnosands
 simple_design_diagnosands <- 
--- formalizing-mida.R
+++ formalizing-mida.R
@@ -133,3 +133,42 @@ gulzar_khan_design <-
     clusters = villages,
     model = lh_robust
   )
+
+design <-
+  declare_population(N = 100,
+                     X = rbinom(N, 1, 0.3),
+                     U = rnorm(N)) +
+  declare_potential_outcomes(Y ~ Z + X + U) +
+  declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0)) +
+  declare_sampling(strata = X, prob = 1) + 
+  declare_assignment(blocks = X, block_prob = c(0.1, 0.5)) +
+  declare_estimator(Y ~ Z, estimand = "ATE", label = "Naive DIM") +
+  declare_estimator(Y ~ Z,
+                    blocks = X,
+                    estimand = "ATE",
+                    label = "Blocked DIM")
+
+dag <- dagify(
+  Y ~ Z + X + U,
+  Z ~ X + S,
+  S ~ X
+)
+
+nodes <-
+  tibble(
+    name = c("Y", "S", "Z", "U", "X"),
+    label = c("Y", "S", "Z", "U", "X"),
+    annotation = c(
+      "**Outcome**<br>",
+      "**Sampling**",
+      "**Random assignment**<br>",
+      "**Unknown heterogeneity**",
+      "**villages**<br>Stratification, cluster assignment"),
+    x = c(5, 1, 3, 5, 1),
+    y = c(2.5, 2.5, 2.5, 4, 4), 
+    nudge_direction = c("S", "S", "S", "N", "N"),
+    answer_strategy = "uncontrolled"
+  )
+ggdd_df <- make_dag_df(dag, nodes, design)
+
+base_dag_plot %+% ggdd_df
