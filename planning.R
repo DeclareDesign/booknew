0ba2393581a95d3e85342e158d1fa89a3ac56c99
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-31 18:28:25 +0000

    Deploy DeclareDesign/book to github.com/DeclareDesign/book.git:gh-pages

--- planning.R
+++ planning.R
@@ -15,12 +15,12 @@ model <-
     N = 800,
     female = rbinom(N, 1, prob = 0.51),
     lgbtq = rbinom(N, 1, prob = 0.05),
+    linked_fate = sample(1:5, N, replace = TRUE, prob = c(0.05, 0.05, 0.15, 0.25, 0.5)),
     age = sample(18:80, N, replace = TRUE),
     religiosity = sample(1:6, N, replace = TRUE),
     income = sample(1:12, N, replace = TRUE),
     college = rbinom(N, 1, prob = 0.5),
     blm_familiarity = sample(1:4, N, replace = TRUE),
-    linked_fate = sample(1:5, N, replace = TRUE, prob = c(0.05, 0.05, 0.15, 0.25, 0.5)),
     U = runif(N),
     blm_support_latent = rescale(
       U + 0.1 * blm_familiarity + 0.45 * linked_fate + 0.001 * age + 
@@ -29,8 +29,8 @@ model <-
   declare_potential_outcomes(
     blm_support_Z_general = likert_cut(blm_support_latent),
     blm_support_Z_nationalism = likert_cut(blm_support_latent + 0.01 + linked_fate * 0.01),
-    blm_support_Z_feminism = likert_cut(blm_support_latent + 0.01 + female * 0.05),
-    blm_support_Z_intersectional = likert_cut(blm_support_latent + 0.01 + lgbtq * 0.15)
+    blm_support_Z_feminism = likert_cut(blm_support_latent - 0.02 + female * 0.07),
+    blm_support_Z_intersectional = likert_cut(blm_support_latent  - 0.05 + lgbtq * 0.15)
   )
 
 inquiry <-  
