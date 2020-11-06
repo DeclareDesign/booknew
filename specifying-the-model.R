6e72221f376849cbfc750f5e8d4a81562b66164d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-06 14:42:37 +0000

    Deploy DeclareDesign/book to github.com/DeclareDesign/book.git:gh-pages

--- specifying-the-model.R
+++ specifying-the-model.R
@@ -46,6 +46,45 @@ ggplot(gg_df, aes(x = x, y = y, xend = xend, yend = yend)) +
 
 g
 
+a_blue <- "#0072B2"
+a_gray <- "grey80"
+a_red <- "red"
+
+dag <-
+  dagify(Y ~ M + X1 + X3 + U,
+         M ~ X2,
+         X2 ~ Z + X1,
+         K ~ X2 + U,
+         latent = c("U", "X1"))
+
+gg_df <-
+  tidy_dagitty(dag,
+               layout = "manual",
+               x = c(M = 3, U = 4, X1 = 3, X2 = 2, X3 = 4, Z = 1, Y = 4, K = 3),
+               y = c(M = 1, U = 0, X1 = 2, X2 = 1, X3 = 2, Z = 1, Y = 1, K = 0))
+
+gg_df <-
+  gg_df %>%
+  mutate(
+    color = case_when(
+      name == "U" ~ a_gray,
+      name == "X1" ~ a_gray,
+      name == "X2" ~ a_red,
+      name == "Y" ~ a_blue
+    )
+  )
+
+
+g <-
+ggplot(gg_df, aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_dag_node(aes(color = color)) +
+  scale_color_identity() +
+  geom_dag_text(color = "black", family = "Helvetica") +
+  geom_dag_edges() +
+  theme_dag()
+
+g
+
 diff_in_cates <- 0.5
 design <-
   declare_population(N = 100,
