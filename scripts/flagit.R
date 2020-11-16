719e1547fc8f9a2c7ed6db9c009d8715511f64b9
macartan <macartan@gmail.com>
2020-11-16 10:35:26 +0100

    Update flagit.R

--- scripts/flagit.R
+++ scripts/flagit.R
@@ -2,7 +2,7 @@ flagit <- function() if(exists("show_flags")) if(show_flags) {
     if (knitr::is_latex_output()) {
       sprintf("\\textcolor{%s}{%s}", "red", "FLAG! \\")
     } else if (knitr::is_html_output()) {
-      sprintf("<span style='color: %s;'>%s</span>", "red", "<small>FLAG</small>")
+      sprintf("<span style='color: %s;'>%s</span>", "red", "<small>[FLAG]</small>")
     } 
 }
 
