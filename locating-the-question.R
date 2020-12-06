762aefbeee8f0f9828cb0b98d608e6c48e8847cc
graemeblair <graemeblair@users.noreply.github.com>
2020-12-06 04:14:42 +0000

    Deploying to gh-pages from  @ abb9ff13828c2ad11a4e8feaba49cc5e746ad1fa 🚀

---
+++ locating-the-question.R
@@ -0,0 +1,91 @@
+# ---
+# Locating the question
+# --- 
+
+packages <- c("tidyverse", "DeclareDesign")
+lapply(packages, require, character.only = TRUE)
+
+library(metafor)
+library(car)
+
+
+# An estimator that estimates on testing data only if 
+# estimates on training data were significant
+
+new_estimator <- function(data){
+    with(data, data.frame(
+      estimate = ifelse(train_p[1] <= .05, 
+                        coef(lm(Y ~ Z*X, subset = !train))[4], 
+                        NA),
+      p.value = ifelse(train_p[1] <= .05, 
+                       coef(summary(lm(Y ~ Z * X, subset = !train)))[4,4],
+                       NA),
+      term = "Z:X",
+      stringsAsFactors = FALSE))}
+
+# An estimator that estimates on all data only if 
+# estimates on training data were significant
+
+comparison_estimator <- function(data){
+  with(data, data.frame(
+    estimate = ifelse(all_p[1] < .05, coef(lm(Y ~ Z*X))[4], NA),
+    p.value = ifelse(all_p[1] < .05, all_p[1], NA),
+    term = "Z:X",
+    stringsAsFactors = FALSE))}
+
+# The design
+
+discovery <- 
+  
+  declare_population(
+    N = 200, 
+    X = sample(1:N %% 2)==1,
+    het_effect = sample(c(0,.5),1,TRUE),
+    train = sample(1:N %% 2)==1,
+    u = rnorm(N)) +
+  
+  declare_potential_outcomes(Y ~ Z + het_effect * Z * X + u) + 
+    declare_estimand(ATE = mean(Y_Z_1 - Y_Z_0)) +
+    declare_assignment() +
+    reveal_outcomes() +
+    
+  # Main analysis
+  declare_estimator(Y ~ Z, estimand = "ATE", label = "Main") +
+  
+  # Exploration
+  declare_step(
+      train_p = (lm_robust(Y ~ Z * X, subset = train) %>% tidy())[4,4],
+      all_p   = (lm_robust(Y ~ Z * X) %>% tidy())[4,4],
+      handler = fabricate) +
+    
+  declare_estimand(
+    het = mean(Y_Z_1[X] - Y_Z_0[X]) - mean(Y_Z_1[!X] - Y_Z_0[!X])) +
+  
+  # Handler estimates only if p low in training group  
+  declare_estimator(
+    handler = tidy_estimator(new_estimator), 
+    estimand = "het",
+    label = "Discovery") +
+    
+  declare_estimator(
+    handler = tidy_estimator(comparison_estimator), 
+    estimand = "het",
+    label = "Comparison")
+
+discovery <- set_diagnosands(discovery, declare_diagnosands(
+  bias = mean((estimate - estimand), na.rm = TRUE),
+  RMSE = sqrt(mean((estimate - estimand)^2, na.rm = TRUE)),
+  frequency = mean(!is.na(estimate)),
+  false_pos = mean(p.value[estimand == 0] < 0.05, na.rm = TRUE),
+  false_neg = 1 - mean(p.value[estimand != 0] < 0.05, na.rm = TRUE)))
+
+
+
+
+
+
+diagnosis %>%
+  reshape_diagnosis() %>% select(-Term, - 'N Sims') %>%
+  kable(digits = 3, booktabs = TRUE, caption = "Complete random sampling design diagnosis")
+
+kable(reshape_diagnosis(diagnosis)[c("Estimator Label","Term","Bias","RMSE","Frequency","False Pos","False Neg")])
--- structural-estimation.R
+++ structural-estimation.R
@@ -5,4 +5,112 @@
 packages <- c("tidyverse", "DeclareDesign")
 lapply(packages, require, character.only = TRUE)
 
-# load packages for this section here. note many (DD, tidyverse) are already available, see scripts/package-list.R
+library(metafor)
+library(car)
+
+
+structural_estimator <- function(data, pi, y, chi = 3/4){
+    
+    # Define negative log likelihood as a function of k, d and q
+    LL  <- function(k, d, q) {
+      m <- with(data, y(Z, d))
+      R <- q * dbeta(data[pi][[1]], k * chi, k * (1- chi)) +
+        (1 - q) * dbeta(data[pi][[1]], k * m, k * (1 - m))
+      - sum(log(R))
+    }
+
+  # Estimation
+  M <- mle2(
+    LL,
+    method = "L-BFGS-B",
+    start = list(k = 2, d = 0.50,  q = 0.50),
+    lower = c(k = 1,    d = 0.01,  q = 0.01),
+    upper = c(k = 1000, d = 0.99,  q = 0.99)
+  )
+  
+  # Format output from estimation
+  out <- data.frame(coef(summary(M)), outcome = pi)
+  
+  names(out) <- c("estimate", "std.error", "statistic", "p.value", "outcome")
+  
+  # Use estimates of q and delta to predict average treatment effects (ATEs)
+  # Predicted ATE for n=2
+  out[4, 1] <- (1 - out["q", "estimate"]) * (2 * out["d", "estimate"] - 1)
+  
+  # Predicted ATE for n=infinity
+  out[5, 1] <- (1 - out["q", "estimate"]) * (2 * out["d", "estimate"] /
+                                               (1 + out["d", "estimate"]) - 1)
+  
+  out
+}
+
+# Declare the design ----------------------------------------------------------
+
+# Define parameter values:
+d = 0.8       # True delta (unknown)
+k = 6         # Parameter to governance variance (unknown)
+q = 0.5       # Share of behavioral types in the population (unknown)
+chi = 0.75    # Price paid by norm following ("behavioral" customers) (known)
+
+# Design declaration:
+
+design <- 
+  
+  # Define the population: indicator for behavioral type (norm = 1)
+  declare_population(N = 500, norm = rbinom(N, 1, q)) +
+  
+  # Define mean potential outcomes for n = 2 
+  declare_potential_outcomes(
+    pi_two ~ norm*chi + (1-norm)*(Z*d + (1-Z)*(1-d))) +
+  
+  # Define mean potential outcomes for n = infinity
+  declare_potential_outcomes(
+    pi_inf ~ norm*chi + (1-norm)*(Z*d/(1+d) + (1-Z)*(1-d/(1+d)))) +
+  
+  # Define estimands (quantities we want to estimate)
+  declare_estimand(ATE_two = mean(pi_two_Z_1 - pi_two_Z_0), # ATE n = 2
+                   ATE_inf = mean(pi_inf_Z_1 - pi_inf_Z_0), # ATE n = infinity
+                   k = k,                                   # kappa
+                   d = d,                                   # delta
+                   q = q) +                                 # q
+
+  # Declare assignment process 
+  declare_assignment() +
+  
+  # Declare revealed potential outcomes
+  reveal_outcomes(pi_two, Z) + reveal_outcomes(pi_inf, Z) +
+  
+  # Get draws from beta distribution given means for n = 2 and n = infinity
+  declare_measurement(
+    pi_two_obs = rbeta(N, pi_two*k, (1-pi_two)*k),      
+    pi_inf_obs = rbeta(N, pi_inf*k, (1-pi_inf)*k)) +
+  
+  # Declare estimators
+  # Difference-in-means for n = 2
+  declare_estimator(pi_two_obs ~ Z, estimand = "ATE_two", label = "DIM_two") +
+  
+  # Difference-in-means for n = infinity
+  declare_estimator(pi_inf_obs ~ Z, estimand = "ATE_inf", label = "DIM_inf") +
+  
+  # MLE for n = 2
+  declare_estimator(handler = tidy_estimator(structural_estimator), 
+                    pi = "pi_two_obs", 
+                    y = function(Z, d) Z * d + (1 - Z) * (1 - d), 
+                    estimand = c("k","d", "q", "ATE_two", "ATE_inf"), 
+                    label = "Struc_two") +
+  
+  # MLE for n = infinity
+  declare_estimator(handler = tidy_estimator(structural_estimator),
+                    pi = "pi_inf_obs", 
+                    y = function(Z, d) Z*d/(1+d) +  (1-Z)*(1-d/(1+d)),
+                    estimand = c("k","d","q","ATE_two", "ATE_inf"), 
+                    label = "Struc_inf") 
+
+
+
+
+
+diagnosis %>%
+  reshape_diagnosis() %>%
+  select(-Coverage, -'Mean Se', - 'N Sims', - 'Type S Rate', - Term) %>%
+  kable(digits = 3, booktabs = TRUE, caption = "Complete random sampling design diagnosis")
