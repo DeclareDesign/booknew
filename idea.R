9cb45f8c72d31abd982d7dfe7d28cf4a845db69a
macartan <macartan@gmail.com>
2021-01-19 15:06:53 +0000

    Deploying to gh-pages from  @ 493f9c7aa1b3640367d6ace8cfcb59a71531c186 🚀

--- idea.R
+++ idea.R
@@ -6,3 +6,57 @@ packages <- c("tidyverse", "DeclareDesign")
 lapply(packages, require, character.only = TRUE)
 
 # load packages for this section here. note many (DD, tidyverse) are already available, see scripts/package-list.R
+
+## 
+## **Problem statement**
+
+## 
+
+## **Model**
+
+## 
+
+## What is the population of units of interest? How many are there? What are their characteristics?
+
+## 
+
+## What is your theoretical model?
+
+## - Draw a graphical representation of this part of the model (see Chapter 5). What are the variables that matter? Are they causally connected? In which direction? What parts are you confident in based on past work or your observations and what parts do you want to learn about from the design?
+
+## 
+
+## **Inquiry**
+
+## 
+
+## What are your research questions?
+
+## - If you have a model (above), express your question in terms of the variables in your model.
+
+## - If you started with a model, what are all of the inquiries that are implied by the model, that would provide empirical evidence to support or falsify your theoretical model?
+
+## 
+
+## Why is this question important? If we learned the answer, what part of the world would we understand better (in other words, what subgraph of a model of the world would we know something new about or be more confident in?).
+
+## 
+
+## **Data strategy**
+
+## 
+## Do you have a treatment assignment scheme in mind? At what level will you randomize? How many units can be assigned to each condition?
+
+## 
+## Do you have a measurement scheme in mind? What variables will you measure? With what instrument?
+
+## 
+## What is the ideal experiment you would run, if there were no constraints? (This exercise may help to stimulate ideas both about experiments you could feasibly run and what observational designs might mimic this ideal experiment.)
+
+## 
+
+## **Answer strategy**
+
+## 
+## How will you use the data that results from your proposed data strategy to produce answers to your inquiry?
+
