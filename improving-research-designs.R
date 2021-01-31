36bb31cf6cf836b6ea5d5e41e85806d3a5890b52
acoppock <acoppock@users.noreply.github.com>
2021-01-31 01:07:17 +0000

    Deploying to gh-pages from  @ 71efa7fada2b23fb56808c5ad394019c205bfad7 🚀

--- improving-research-designs.R
+++ improving-research-designs.R
@@ -9,26 +9,46 @@ lapply(packages, require, character.only = TRUE)
 
 model <-
   declare_model(
-    villages = add_level(N = 192),
-    citizens = add_level(N = 100, 
-                         U = runif(N),
-                         Y_Z_neutral = U,
-                         Y_Z_personal = Y_Z_neutral + 0.02,
-                         Y_Z_social = Y_Z_neutral + 0.03))
+    villages = add_level(N = 500, U_village = rnorm(N, sd = 0.1)),
+    citizens = add_level(
+      N = 100, 
+      U_citizen = rnorm(N),
+      potential_outcomes(
+        Y ~ pnorm(
+          U_citizen + U_village + 
+            0.1 * (Z == "personal") + 0.15 * (Z == "social")),
+          conditions = list(Z = c("neutral", "personal", "social"))
+        )))
 
 inquiry <- declare_inquiry(
   ATE_personal = mean(Y_Z_personal - Y_Z_neutral),
   ATE_social = mean(Y_Z_social - Y_Z_neutral)
 )
 
+n_villages <- 192
+citizens_per_village <- 48
+
 data_strategy <-
-  declare_sampling(S = strata_rs(strata = villages, n = 48), legacy = FALSE) +
+  declare_sampling(
+    S_village = cluster_rs(clusters = villages, n = n_villages),
+    filter = S_village == 1,
+    legacy = FALSE) +
+  
+  declare_sampling(
+    S_citizen = strata_rs(strata = villages, n = citizens_per_village),
+    filter = S_citizen == 1,
+    legacy = FALSE) +
+  
   declare_assignment(
-  	Z = cluster_ra(clusters = villages, 
-  	               conditions = c("neutral", "personal", "social")),
+  	Z = cluster_ra(
+  	  clusters = villages, 
+  	  conditions = c("neutral", "personal", "social")),
   	legacy = FALSE) + 
-  declare_measurement(Y_latent = reveal_outcomes(Y ~ Z),
-                      Y_observed = if_else(Y_latent > 0.97, 1, 0))
+  
+  declare_measurement(
+    Y_latent = reveal_outcomes(Y ~ Z),
+    Y_observed = rbinom(N, 1, prob = Y_latent)
+  )
 
 answer_strategy <- 
   declare_estimator(Y_observed ~ Z, term = c("Zpersonal", "Zsocial"), 
@@ -65,7 +85,8 @@ base_dag_plot %+% ggdd_df + coord_fixed(ylim = c(2.05, 4.6), xlim = c(0.25 - eps
 diagnosands <- declare_diagnosands(
   bias = mean(estimate - estimand),
   rmse = sqrt(mean((estimate - estimand)^2)),
-  power = mean(p.value <= 0.05)
+  power = mean(p.value <= 0.05),
+  cost = mean(10 * n_villages + 1 * n_villages * citizens_per_village)
 )
 
 
@@ -75,3 +96,7 @@ diagnosands <- declare_diagnosands(
 get_diagnosands(diagnosis) %>% 
   select(inquiry_label, estimator_label, bias, rmse, power) %>%
   kable(digits = 3, caption = "Diagnosis of the simplified Gulzar-Khan design.", booktabs = TRUE)
+
+designs <- redesign(design, n_villages = c(192, 295, 397, 500), citizens_per_village = c(25, 50, 75, 100))
+
+dx <- diagnose_design(designs, diagnosands = diagnosands, sims = 500)
