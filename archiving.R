ea37e85f27d9bc0e90ed2b130dee4bc1c97804a1
acoppock <acoppock@users.noreply.github.com>
2021-02-28 00:57:57 +0000

    Deploying to gh-pages from  @ 10effdaf2a160d0c2c13c53b781ff66ac5c6e884 🚀

--- archiving.R
+++ archiving.R
@@ -6,3 +6,5 @@ packages <- c("tidyverse", "DeclareDesign")
 lapply(packages, require, character.only = TRUE)
 
 # load packages for this section here. note many (DD, tidyverse) are already available, see scripts/package-list.R
+
+knitr::include_graphics("figures/file_structure.png")
